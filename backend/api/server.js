4f94d1a7bf1174399b82e23eca3a0fce331fb6b9
Mohit Bhalotia <mohitkumarbhalotia420@gmail.com>
2025-03-23 03:16:00 +0530

    Added cors

--- backend/api/server.js
+++ backend/api/server.js
@@ -14,10 +14,7 @@ const escrowRoutes = require('../src/routes/escrow');
 const app = express();
 
 // Middleware
-app.use(cors({
-  origin: process.env.FRONTEND_URL || 'http://localhost:5000',
-  credentials: false
-}));
+app.use(cors());
 app.use(express.json());
 
 // Routes
